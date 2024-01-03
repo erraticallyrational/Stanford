031391d80f208b8d0abaabf2558a7fe589214a2a
erraticallyrational <shaquilkcampbell@gmail.com>
2024-01-03 00:00:28 -0500

    created an array for the tiles

--- Stanford/ContentView.swift
+++ Stanford/ContentView.swift
@@ -8,12 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis: [String] = ["👻","🎃","🕸️","👹"]
+    
     var body: some View {
         HStack{
-       CardView(isFaceUp: true)
-            CardView(isFaceUp: true)
-            CardView(isFaceUp: true)
-            CardView(isFaceUp: true)
+            ForEach(emojis.indices, id: \.self) { index in
+                CardView(content: emojis[index])
+            }
         }
         .foregroundColor(.orange)
         .padding()
@@ -21,15 +22,15 @@ struct ContentView: View {
 }
 
 struct CardView: View {
-   @State  var isFaceUp = false
-    
+    let content: String
+   @State  var isFaceUp = true
     var body: some View {
         ZStack {
             let base =  RoundedRectangle(cornerRadius:12)
             if isFaceUp{
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
-                Text("👻").font(.largeTitle)
+                Text(content).font(.largeTitle)
             }else {
                 base.fill()
             }
