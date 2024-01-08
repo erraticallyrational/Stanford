f920f77d18abd1da028f770874baacb347e3e30e
erraticallyrational <shaquilkcampbell@gmail.com>
2024-01-07 22:06:08 -0500

    finished classed 2

--- Stanford/ContentView.swift
+++ Stanford/ContentView.swift
@@ -8,35 +8,83 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis: [String] = ["👻","🎃","🕸️","👹"]
+    let emojis: [String] = ["👻","🎃","🕸️","👹","⚡️","☠️","👺","🧙🏿","🕷️","🍭","💀","🍬"]
     
+    @State var cardCount: Int = 4
     var body: some View {
-        HStack{
-            ForEach(emojis.indices, id: \.self) { index in
-                CardView(content: emojis[index])
+        VStack {
+            ScrollView{
+                cards
             }
+            Spacer()
+            cardCountAdjusters
+            
         }
-        .foregroundColor(.orange)
         .padding()
     }
-}
-
-struct CardView: View {
-    let content: String
-   @State  var isFaceUp = true
-    var body: some View {
-        ZStack {
-            let base =  RoundedRectangle(cornerRadius:12)
-            if isFaceUp{
-                base.fill(.white)
-                base.strokeBorder(lineWidth: 2)
-                Text(content).font(.largeTitle)
-            }else {
-                base.fill()
+    
+    
+    
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 65)),GridItem(), GridItem()]){
+            ForEach(0..<cardCount, id: \.self) { index in
+                CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit
+                    )
             }
-        }
-        .onTapGesture {
-            isFaceUp.toggle()
+        }.foregroundColor(.orange)
+    }
+    
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount > emojis.count)
+    }
+    
+    var cardRemover: some View {
+        return cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+    }
+    
+    var cardAdder: some View {
+        return cardCountAdjuster(by: +1, symbol: "rectangle.stack.badge.plus.fill")
+    }
+    
+    
+    var cardCountAdjusters: some View {
+        HStack{
+            cardRemover
+            Spacer()
+            cardAdder
+            
+        } .imageScale(.large)
+            .font(.largeTitle)
+    }
+    
+    
+    
+    
+    
+    struct CardView: View {
+        let content: String
+        @State  var isFaceUp = true
+        var body: some View {
+            ZStack {
+                let base =  RoundedRectangle(cornerRadius:12)
+                Group{
+                    base.fill(.white)
+                    base.strokeBorder(lineWidth: 2)
+                    Text(content).font(.largeTitle)
+                }
+                .opacity(isFaceUp ? 1 :0 )
+                base.fill().opacity(isFaceUp ? 0 :1 )
+            }
+            .onTapGesture {
+                isFaceUp.toggle()
+            }
         }
     }
 }
